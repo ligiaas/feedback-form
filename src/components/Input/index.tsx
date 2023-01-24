c5baedb50ce4e557fbd60e8a6e4821da53d3cef7
Ligia de Almeida <ligiaas.dev@gmail.com>
2023-01-24 10:58:13 -0300

    feat: creating input component

---
+++ src/components/Input/index.tsx
@@ -0,0 +1,8 @@
+import './styles.css';
+import * as Types from '../../types/input';
+
+export const Input: React.FC<Types.InputProps> = ({
+  id,
+  placeholder,
+  type,
+}) => <input id={id} placeholder={placeholder} type={type} />;
