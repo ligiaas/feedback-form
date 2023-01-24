5a894c8b9affe3199134a0cc1a5545bf047b1586
Ligia de Almeida <ligiaas.dev@gmail.com>
2023-01-24 11:02:37 -0300

    feat: adding news components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { createCtx } from './contexts/inputContext';
+import Form from './components/Form';
+
+const [ctx, TextProvider] = createCtx('');
+export const TextContext = ctx;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TextProvider>
+      <Form />
+    </TextProvider>
   );
 }
 
