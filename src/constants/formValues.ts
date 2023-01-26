02da241369334d49fc9546170413a0fd9e9d8157
Ligia de Almeida <ligiaas.dev@gmail.com>
2023-01-26 17:39:56 -0300

    feat: making initialValues global

--- src/constants/formValues.ts
+++ src/constants/formValues.ts
@@ -1,2 +1,8 @@
 export const emailValidation =
   /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
+
+export const initialValues = {
+  name: '',
+  email: '',
+  message: '',
+};
